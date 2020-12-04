7cc45a07423e7ded5ff3dcdea0f59bcb6c69e07d
zhengyis123123123123 <zhengyisong960221@163.com>
2020-12-04 14:53:43 +0800

    -郑易松12月4号第一次提交

---
+++ 郑易松/12月4号/pch.cpp
@@ -0,0 +1,77 @@
+﻿// pch.cpp: 与预编译标头对应的源文件
+
+#include "pch.h"
+
+// 经纬度转栅格坐标	(李洪烈提供函数)
+// 输入：经纬度(船位坐标)、静态障碍物结构体
+// 输出：新的坐标点结构体
+Point* LonLatToCoordinate(double longitude, double latitude, GridInfo* StaticGrid)
+{
+	Point* point = new Point(-1, -1);
+	if (StaticGrid == NULL || StaticGrid->xNumber == -1 || StaticGrid->yNumber == -1) {
+		TRACE("静态数据传输错误，为空或越界\n");
+	}
+	int x = (int)((longitude - StaticGrid->StartPoint.x) / (StaticGrid->offsetValue.x / 60.f) + 0.5);
+	int y = (int)((latitude - StaticGrid->StartPoint.y) / (StaticGrid->offsetValue.y / 60.f) + 0.5);
+	if (x < 0 || x >= StaticGrid->xNumber || y < 0 || y >= StaticGrid->yNumber) {
+		TRACE("数据传输错误，传入的数据越界\n");
+	}
+	point->x = x;
+	point->y = y;
+	return point;
+}
+
+// 方格坐标转纬度
+double GetLonLatFromIDLat(GridInfo* chart, int x, int y)
+{
+	if (chart == NULL)
+		return 0.0;
+	if (chart->xNumber == -1 || chart->yNumber == -1)
+		return 0.0;
+	double lat = 0.0;
+	if (y == 0) {
+		lat = 0.0;
+	}
+	lat = chart->StartPoint.y + chart->offsetValue.y / 60 * y;
+	return lat;
+}
+
+// 方格坐标转经度
+double GetLonLatFromIDLon(GridInfo* chart, int x, int y)
+{
+	if (chart == NULL)
+		return 0.0;
+	if (chart->xNumber == -1 || chart->yNumber == -1)
+		return 0.0;
+	double lon = 0.0;
+	if (x == 0) {
+		lon = 0.0;
+	}
+	lon = chart->StartPoint.x + chart->offsetValue.x / 60 * x;
+	return lon;
+}
+
+// 任务2020/12/4号下达
+// Bresenham算法
+// 输入参数
+// 起始点X、Y坐标
+// 终点X、Y坐标
+// 返回值：链表，由起始点到终点，不带头节点
+Point* CalculateBresenham(int startX, int startY, int endX, int endY) {
+	return NULL;
+}
+
+// 当使用预编译的头时，需要使用此源文件，编译才能成功。
+RouteLine* CbbpLine(boat* myBoat, RouteLine* lineData, GridInfo* StaticGrid) {
+
+	return NULL;
+}
+
+// 删除旧航线
+void ReleaseRouteLine(RouteLine* pRoute)
+{
+	if (pRoute != NULL)
+	{
+		delete pRoute;
+	}
+}
---
+++ 郑易松/12月4号/pch.h
@@ -0,0 +1,87 @@
+﻿// pch.h: 这是预编译标头文件。
+// 下方列出的文件仅编译一次，提高了将来生成的生成性能。
+// 这还将影响 IntelliSense 性能，包括代码完成和许多代码浏览功能。
+// 但是，如果此处列出的文件中的任何一个在生成之间有更新，它们全部都将被重新编译。
+// 请勿在此处添加要频繁更新的文件，这将使得性能优势无效。
+
+#ifndef PCH_H
+#define PCH_H
+
+// 添加要在此处预编译的标头
+#include "framework.h"
+/*
+以下是交换使用的数据结构
+作者：李洪烈
+时间：2020/6/1
+*/
+typedef struct _DoublePoint {
+	double x;					//经度
+	double y;					//纬度
+}DoublePoint;
+
+typedef struct _boat
+{
+	double longitude;//经度->0
+	double latitude;//纬度->1
+	double azimuth;//航向->2:北顺时针转动多少度
+	double speed;//航速->3
+	char Name[32];
+	_boat* next;
+	_boat()
+	{
+		longitude = 181;
+		latitude = 91;
+		azimuth = 0;
+		speed = 0;
+
+		memset(Name, 0, 32);
+		next = NULL;
+	}
+	~_boat()
+	{
+		if (next != NULL)
+			delete next;
+	}
+}boat;
+
+typedef struct _RouteLine {
+	double longitude;//经度->0
+	double latitude;//纬度->1
+	double XTE;
+	int number;//航线序号 
+	_RouteLine* next;
+	_RouteLine()
+	{
+		next = NULL;
+	}
+	~_RouteLine()
+	{
+		if (next != NULL)
+			delete next;
+	}
+}RouteLine;
+
+typedef struct  _GridInfo
+{
+	DoublePoint StartPoint;
+	DoublePoint EndPoint;
+	DoublePoint offsetValue;
+	int xNumber;
+	int yNumber;
+	unsigned char* GridInfo;
+}GridInfo;
+
+/*
+作者:郑易松
+定义的数据结构
+时间：2020/06/07
+*/
+struct Point
+{
+	int x, y;																					//点坐标，这里为了方便按照C++的数组来计算，x代表横排，y代表竖列, length列表长度
+	Point(int _x, int _y) :x(_x), y(_y)
+	{
+	}
+};
+
+#endif //PCH_H
